3037e004b7ef6e57814b2bc39777352dedd2443b
Tiziano <tizianocitro98@gmail.com>
2023-04-14 09:41:39 +0000

    Fixed documentation sidebar

--- cs-connect/webapp/src/components/documentation/documentation_sidebar.tsx
+++ cs-connect/webapp/src/components/documentation/documentation_sidebar.tsx
@@ -1,43 +1,44 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {getCurrentTeamId} from 'mattermost-redux/selectors/entities/teams';
 import {useSelector} from 'react-redux';
 import {useLocation} from 'react-router-dom';
 
-import {buildMap, useReservedCategoryTitleMapper} from 'src/hooks';
+import {useReservedCategoryTitleMapper} from 'src/hooks';
 import Sidebar from 'src/components/sidebar/sidebar';
 import {pluginUrl} from 'src/browser_routing';
 import {DOCUMENTATION_PATH} from 'src/constants';
 
-const idsMap = buildMap([
-    {key: 'about', value: {id: 'about-the-platform', name: 'About the platform'}},
-    {key: 'mechanism', value: {id: 'hyperlinking-mechanism', name: 'Hyperlinking Mechanism'}},
-]);
+import {DocumentationMapItem} from './documentation';
 
-const useLHSData = () => {
+type Props = {
+    items: DocumentationMapItem[];
+};
+
+const useLHSData = (items: DocumentationMapItem[]) => {
     const {hash: urlHash} = useLocation();
     const normalizeCategoryName = useReservedCategoryTitleMapper();
     const icon = 'icon-play-outline';
 
-    // TODO: use the same mechanism as the rhs to remove the active class
+    // Use the same mechanism as the rhs to remove the active class
     // when the group is closed and opened the items become all active
     // by detecting the change in class you can remove the active class
     // to all items without the id equals to the hash
-    useEffect(() => {
-        idsMap.forEach(({id}, _) => {
-            const item = document.getElementById(`sidebarItem_${id}`) as HTMLElement;
-            item.classList.remove('active');
-            if (urlHash === `#${id}`) {
-                item.classList.add('active');
-            }
-        });
-    }, [urlHash]);
+    // useEffect(() => {
+    //     itemsMap.forEach(({id}, _) => {
+    //         const item = document.getElementById(`sidebarItem_${id}`) as HTMLElement;
+    //         item.classList.remove('active');
+    //         if (urlHash === `#${id}`) {
+    //             item.classList.add('active');
+    //         }
+    //     });
+    // }, [urlHash]);
 
-    const documentationItems = Array.from(idsMap.values()).map(({id, name}) => ({
+    const documentationItems = items.map(({id, name, path}) => ({
         areaLabel: name,
         display_name: name,
         id,
         icon,
-        link: pluginUrl(`/${DOCUMENTATION_PATH}#${id}`),
+        link: pluginUrl(`/${DOCUMENTATION_PATH}/${path}#${id}`),
         isCollapsed: false,
         className: '',
     }));
@@ -53,9 +54,9 @@ const useLHSData = () => {
     return {groups, ready: true};
 };
 
-const DocumentationSidebar = () => {
+const DocumentationSidebar = ({items}: Props) => {
     const teamID = useSelector(getCurrentTeamId);
-    const {groups, ready} = useLHSData();
+    const {groups, ready} = useLHSData(items);
 
     if (!ready) {
         return (
