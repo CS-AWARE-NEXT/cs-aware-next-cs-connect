fb98e3b462e6fbb29cf710fba5b7d3f415a1b332
Tiziano <tizianocitro98@gmail.com>
2023-04-21 14:53:48 +0000

    Added argument for configuration file

--- cs-connect/server/config/platform_service.go
+++ cs-connect/server/config/platform_service.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"fmt"
 	"path/filepath"
+	"strings"
 
 	"github.com/pkg/errors"
 
@@ -9,20 +11,30 @@ import (
 )
 
 type PlatformService struct {
-	api plugin.API
+	api                   plugin.API
+	configFileName        string
+	defaultConfigFileName string
 }
 
 // NewPlatformService returns a new platform config service
-func NewPlatformService(api plugin.API) *PlatformService {
+func NewPlatformService(api plugin.API, configFileName, defaultConfigFileName string) *PlatformService {
 	return &PlatformService{
-		api: api,
+		api:                   api,
+		configFileName:        configFileName,
+		defaultConfigFileName: defaultConfigFileName,
 	}
 }
 
 func (s *PlatformService) GetPlatformConfig() (*PlatformConfig, error) {
+	if strings.TrimSpace(s.configFileName) == "" {
+		s.api.LogInfo("Config file is not specified, falling back to default")
+		s.configFileName = s.defaultConfigFileName
+	}
+	s.api.LogInfo("Loading config file", "name", s.configFileName)
+	configFilePath := fmt.Sprintf("config/%s", s.configFileName)
 	bundlePath, err := s.api.GetBundlePath()
 	if err != nil {
 		return nil, errors.Wrapf(err, "unable to get bundle path")
 	}
-	return getPlatformConfig(filepath.Join(bundlePath, "config/config.yml"))
+	return getPlatformConfig(filepath.Join(bundlePath, configFilePath))
 }
