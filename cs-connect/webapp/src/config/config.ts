a30e72039a950ddb4a2202338517c6d0834c1465
Tiziano <tizianocitro98@gmail.com>
2023-05-04 08:18:04 +0000

    Added functions to manipulate configuration

--- cs-connect/webapp/src/config/config.ts
+++ cs-connect/webapp/src/config/config.ts
@@ -3,9 +3,15 @@ import {Organization, PlatformConfig} from 'src/types/organization';
 export const DEFAULT_PLATFORM_CONFIG_PATH = '/configs/platform';
 export const PLATFORM_CONFIG_CACHE_NAME = 'platform-config-cache';
 
+const PATTERN_SYMBOL = ':symbol';
+const PATTERN_PLACEHOLDER = `${PATTERN_SYMBOL}\\(.+?\\)`;
+
 let platformConfig: PlatformConfig = {
     organizations: [],
 };
+
+let symbol = '';
+let pattern: RegExp | null = null;
 
 export const getPlatformConfig = (): PlatformConfig => {
     return platformConfig;
@@ -32,4 +38,20 @@ export const getEcosystem = (): Organization => {
 
 export const getOrganizationById = (id: string): Organization => {
     return getOrganizations().filter((o) => o.id === id)[0];
+};
+
+export const getOrganizationByName = (name: string): Organization => {
+    return getOrganizations().filter((o) => o.name === name)[0];
+};
+
+export const getPattern = (): RegExp => {
+    if (symbol === '' || pattern === null) {
+        symbol = 'hood';
+        pattern = new RegExp(PATTERN_PLACEHOLDER.replace(PATTERN_SYMBOL, symbol), 'g');
+    }
+    return pattern;
+};
+
+export const getSymbol = (): string => {
+    return symbol;
 };
