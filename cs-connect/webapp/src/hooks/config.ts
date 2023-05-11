2edd752aa6469dd8e2c8d00b8e1d388f5b2391f7
Tiziano <tizianocitro98@gmail.com>
2023-05-11 14:46:16 +0000

    Minor refactoring to configuration hooks

--- cs-connect/webapp/src/hooks/config.ts
+++ cs-connect/webapp/src/hooks/config.ts
@@ -1,25 +1,27 @@
+import {useEffect} from 'react';
+
 import {Section} from 'src/types/organization';
 import {getOrganizations} from 'src/config/config';
 import {estimatedOptionsLoadTime} from 'src/constants';
 
 import {formatStringToLowerCase} from './format';
 
-export const getSection = (id: string): Section => {
-    return getOrganizations().
-        map((o) => o.sections).
-        flat().
-        filter((s: Section) => s.id === id)[0];
+export const useHideOptions = () => {
+    useEffect(() => {
+        const [timeouts, intervals] = hideOptions();
+        return () => {
+            timeouts.forEach((timeout) => clearTimeout(timeout));
+            intervals.forEach((interval) => clearInterval(interval));
+        };
+    });
 };
 
-export const isSectionByName = (name: string): boolean => {
-    return getOrganizations().
-        map((o) => o.sections).
-        flat().
-        some((s: Section) => s.name === name);
-};
-
-export const hideOptions = (): NodeJS.Timeout[][] => {
+const hideOptions = (): NodeJS.Timeout[][] => {
     (document.getElementsByClassName('AddChannelDropdown_dropdownButton')[0] as HTMLElement).style.display = 'none';
+    const hiddenIconBox = document.getElementById('hidden-icon')?.parentElement?.parentElement;
+    if (hiddenIconBox) {
+        hiddenIconBox.style.display = 'none';
+    }
 
     const interval = setInterval(() => {
         const indicator = document.getElementById('unreadIndicatorTop');
@@ -47,5 +49,19 @@ export const hideOptions = (): NodeJS.Timeout[][] => {
     }, estimatedOptionsLoadTime);
 
     return [[], [interval]];
+};
+
+export const getSection = (id: string): Section => {
+    return getOrganizations().
+        map((o) => o.sections).
+        flat().
+        filter((s: Section) => s.id === id)[0];
+};
+
+export const isSectionByName = (name: string): boolean => {
+    return getOrganizations().
+        map((o) => o.sections).
+        flat().
+        some((s: Section) => s.name === name);
 };
 
