0f069e777a79ea0db4517ac8208ed2add9d3c13f
Tiziano <tizianocitro98@gmail.com>
2023-05-16 11:48:56 +0000

    Updated indexes

--- cs-connect/webapp/src/helpers/index.ts
+++ cs-connect/webapp/src/helpers/index.ts
@@ -4,3 +4,4 @@ export * from './parser/parser';
 export * from './parser/posts_parser';
 export * from './parser/suggestions_parser';
 export * from './posts';
+export * from './suggestions';
--- cs-connect/webapp/src/hooks/index.ts
+++ cs-connect/webapp/src/hooks/index.ts
@@ -3,4 +3,5 @@ export * from './posts';
 export * from './config';
 export * from './general';
 export * from './routing';
+export * from './suggestions';
 export * from './url';
