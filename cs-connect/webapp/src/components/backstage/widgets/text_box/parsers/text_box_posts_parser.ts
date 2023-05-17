26d3edd424a124aa93e3330a93b32f19cc2cc8a4
Tiziano <tizianocitro98@gmail.com>
2023-05-17 08:09:33 +0000

    Replaced text-box provider with parser

--- cs-connect/webapp/src/components/backstage/widgets/text_box/parsers/text_box_posts_parser.ts
+++ cs-connect/webapp/src/components/backstage/widgets/text_box/parsers/text_box_posts_parser.ts
@@ -3,7 +3,7 @@ import {Widget} from 'src/types/organization';
 import {HyperlinkReference, WidgetHash} from 'src/types/parser';
 
 // Reference example: #description-2ce53d5c-4bd4-4f02-89cc-d5b8f551770c-3-widget
-export const buildTextBoxWidgetId = (
+export const parseTextBoxWidgetId = (
     {section, object}: HyperlinkReference,
     {name}: Widget,
 ): WidgetHash => {
