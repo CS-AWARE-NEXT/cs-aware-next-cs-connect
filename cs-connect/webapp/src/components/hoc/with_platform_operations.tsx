54024ca465c1f9ef44dbb4c68cb248b5137823dc
Tiziano <tizianocitro98@gmail.com>
2023-05-18 11:43:36 +0000

    Added retrieval and storage of channel view information

--- cs-connect/webapp/src/components/hoc/with_platform_operations.tsx
+++ cs-connect/webapp/src/components/hoc/with_platform_operations.tsx
@@ -1,20 +1,40 @@
-import React, {ComponentType} from 'react';
+import React, {ComponentType, useEffect} from 'react';
 import {createPortal} from 'react-dom';
+import {useSelector} from 'react-redux';
+import {getCurrentTeamId} from 'mattermost-webapp/packages/mattermost-redux/src/selectors/entities/teams';
+import {getCurrentChannelId} from 'mattermost-webapp/packages/mattermost-redux/src/selectors/entities/common';
 
 import {useHideOptions, useSuggestions, useUserAdded} from 'src/hooks';
 import Suggestions from 'src/components/chat/suggestions';
-
-import 'src/styles/hyperlink_token_suggestion.scss';
+import {channelNameSelector, teamNameSelector} from 'src/selectors';
 
 const withPlatformOperations = (Component: ComponentType): (props: any) => JSX.Element => {
     return (props: any): JSX.Element => {
         useUserAdded();
         useHideOptions();
-        const [suggestions, isVisible] = useSuggestions();
+        const [suggestions, isVisible, setIsVisible] = useSuggestions();
+
+        const channelId = useSelector(getCurrentChannelId);
+        const teamId = useSelector(getCurrentTeamId);
+        const team = useSelector(teamNameSelector(teamId));
+        const channel = useSelector(channelNameSelector(channelId));
+        useEffect(() => {
+            // localStorage.setItem('teamId', teamId);
+            localStorage.setItem('teamName', team.name);
+            localStorage.setItem('channelId', channelId);
+            localStorage.setItem('channelName', channel.name);
+        }, [channelId, teamId]);
+
         return (
             <>
                 <Component {...props}/>
-                {(suggestions && isVisible) && createPortal(<Suggestions/>, suggestions)}
+                {(suggestions && isVisible) &&
+                    createPortal(
+                        <Suggestions
+                            setIsVisible={setIsVisible}
+                        />,
+                        suggestions,
+                    )}
             </>
         );
     };
