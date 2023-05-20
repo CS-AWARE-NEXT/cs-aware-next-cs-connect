9b3e34b2618896e4659abb5d4194589ad7d69766
Tiziano <tizianocitro98@gmail.com>
2023-05-20 09:24:15 +0000

    Typo in components name

--- cs-connect/webapp/src/components/backstage/widgets/wizard/steps/attachements_step.tsx
+++ cs-connect/webapp/src/components/backstage/widgets/wizard/steps/attachements_step.tsx
@@ -12,47 +12,46 @@ type Props = {
     setWizardData: Dispatch<SetStateAction<any>>,
 };
 
-const AttachedsStep = ({data, setWizardData}: Props) => {
-    const [attacheds, setAttacheds] = useState<string[]>(data);
+const AttachementsStep = ({data, setWizardData}: Props) => {
+    const [attachements, setAttachements] = useState<string[]>(data);
 
     return (
         <Container>
             <div style={{width: '100%'}}>
                 <Button
-                    style={{width: '48%', margin: '2%'}}
-                    icon={<LinkOutlined/>}
                     type='primary'
-                    onClick={() => setAttacheds((prev) => ([...prev, '']))}
+                    icon={<LinkOutlined/>}
+                    style={{width: '48%', margin: '2%'}}
+                    onClick={() => setAttachements((prev) => ([...prev, '']))}
                 >
-                    <FormattedMessage defaultMessage='Add a new link'/>
+                    <FormattedMessage defaultMessage='Add a link'/>
                 </Button>
                 <Button
+                    icon={<FileOutlined/>}
                     style={{width: '48%'}}
                     disabled={true}
-                    icon={<FileOutlined/>}
-                    type='primary'
                 >
-                    <FormattedMessage defaultMessage='Upload a new file'/>
+                    <FormattedMessage defaultMessage='Upload a file'/>
                 </Button>
             </div>
             <List
                 style={{padding: '16px'}}
                 itemLayout='horizontal'
-                dataSource={attacheds}
-                renderItem={(attached, index) => (
+                dataSource={attachements}
+                renderItem={(attachement, index) => (
                     <List.Item>
                         <List.Item.Meta
                             avatar={<Avatar icon={<TagsOutlined/>}/>}
                         />
                         <TextInput
-                            key={'attachment'}
-                            placeholder={'Insert a new attachment'}
-                            value={attached}
+                            key={`attachment-${index}`}
+                            placeholder={'Insert an attachment'}
+                            value={attachement}
                             onChange={(e) => {
-                                const currentAttacheds = cloneDeep(attacheds);
-                                currentAttacheds[index] = e.target.value;
-                                setAttacheds(currentAttacheds);
-                                setWizardData((prev: any) => ({...prev, attacheds: currentAttacheds}));
+                                const currentAttachements = cloneDeep(attachements);
+                                currentAttachements[index] = e.target.value;
+                                setAttachements(currentAttachements);
+                                setWizardData((prev: any) => ({...prev, attachements: currentAttachements}));
                             }}
                         />
                     </List.Item>
@@ -68,4 +67,4 @@ const Container = styled.div`
     margin-top: 24px;
 `;
 
-export default AttachedsStep;
+export default AttachementsStep;
