42a7e36f44ffef3a4410e7cc8a2efc5c97b783ed
Tiziano <tizianocitro98@gmail.com>
2023-05-21 10:40:54 +0000

    Developed wrappers for ecosystem's issues widgets

---
+++ cs-connect/webapp/src/components/backstage/widgets/paginated_table/wrappers/ecosystem_roles_wrapper.tsx
@@ -0,0 +1,89 @@
+import React, {useContext, useEffect, useState} from 'react';
+import {useLocation, useRouteMatch} from 'react-router-dom';
+import qs from 'qs';
+import {Tag} from 'antd';
+
+// import {Client4} from 'mattermost-webapp/packages/mattermost-redux/src/client';
+
+import {buildQuery} from 'src/hooks';
+import {formatName, formatStringToCapitalize} from 'src/helpers';
+import {PaginatedTableData, PaginatedTableRow} from 'src/types/paginated_table';
+import {ecosystemRolesFields, ecosystemRolesWidget} from 'src/constants';
+import PaginatedTable, {fillColumn, fillRow} from 'src/components/backstage/widgets/paginated_table/paginated_table';
+import {Role} from 'src/types/scenario_wizard';
+import {IsEcosystemRhsContext} from 'src/components/rhs/rhs_widgets';
+import {FullUrlContext} from 'src/components/rhs/rhs';
+
+const {CheckableTag} = Tag;
+
+type Props = {
+    name?: string;
+    roles?: Role[];
+};
+
+const EcosystemRolesWrapper = ({
+    name = formatStringToCapitalize(ecosystemRolesWidget),
+    roles = [],
+}: Props) => {
+    const isEcosystemRhs = useContext(IsEcosystemRhsContext);
+    const fullUrl = useContext(FullUrlContext);
+    const {url, params: {sectionId}} = useRouteMatch<{sectionId: string}>();
+    const {search} = useLocation();
+    const queryParams = qs.parse(search, {ignoreQueryPrefix: true});
+    const parentId = queryParams.parentId as string;
+
+    const [data, setData] = useState<PaginatedTableData>({columns: [], rows: []});
+
+    useEffect(() => {
+        const rows = roles.map((role) => {
+            // const user = await Client4.getUser(role.userId);
+            const row: PaginatedTableRow = {
+                id: role.id,
+                name: role.userId,
+                user: role.userId,
+                roles: role.roles,
+            };
+            const query = isEcosystemRhs ? '' : buildQuery(parentId, sectionId);
+            const rowUrl = fullUrl || url;
+            return fillRow(row, '', rowUrl, query);
+        });
+        const columns = ecosystemRolesFields.map((field) => {
+            const column = fillColumn(field);
+            if (column.key !== 'roles') {
+                return column;
+            }
+            return {
+                ...column,
+                render: (text: string, record: PaginatedTableRow) => (
+                    <>
+                        {record.roles.map((role: string) => (
+                            <CheckableTag
+                                key={role}
+                                checked={true}
+                            >
+                                {role}
+                            </CheckableTag>))}
+                    </>
+                ),
+            };
+        });
+        setData({columns, rows});
+    }, [roles]);
+
+    return (
+        <>
+            {(data.columns.length > 0 && data.rows.length > 0) &&
+                <PaginatedTable
+                    data={data}
+                    id={formatName(name)}
+                    isSection={true}
+                    name={name}
+                    sectionId={sectionId}
+                    parentId={parentId}
+                    pointer={true}
+                />}
+        </>
+    );
+};
+
+export default EcosystemRolesWrapper;
