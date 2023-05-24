e6d8b0e80ff1608d352b37a031bdad820fd44cfc
Tiziano <tizianocitro98@gmail.com>
2023-05-24 13:56:56 +0000

    Added value option for text-box widget

--- cs-connect/webapp/src/components/backstage/widgets/text_box/parsers/text_box_posts_parser.ts
+++ cs-connect/webapp/src/components/backstage/widgets/text_box/parsers/text_box_posts_parser.ts
@@ -1,14 +1,29 @@
-import {formatName} from 'src/helpers';
+import {fetchTextBoxData} from 'src/clients';
+import {formatName, formatUrlWithId} from 'src/helpers';
 import {Widget} from 'src/types/organization';
-import {HyperlinkReference, WidgetHash} from 'src/types/parser';
+import {HyperlinkReference, ParseOptions, WidgetHash} from 'src/types/parser';
 
 // Reference example: #description-2ce53d5c-4bd4-4f02-89cc-d5b8f551770c-3-widget
-export const parseTextBoxWidgetId = (
+export const parseTextBoxWidgetId = async (
     {section, object}: HyperlinkReference,
-    {name}: Widget,
-): WidgetHash => {
+    {name, url}: Widget,
+    options?: ParseOptions,
+): Promise<WidgetHash> => {
+    const isValueNeeded = options?.isValueNeeded || false;
+    if (!isValueNeeded) {
+        return {
+            hash: `${formatName(name as string)}-${object?.id}-${section?.id}-widget`,
+            text: name as string,
+        };
+    }
+    let widgetUrl = url as string;
+    if (object) {
+        widgetUrl = formatUrlWithId(widgetUrl, object.id);
+    }
+    const {text} = await fetchTextBoxData(widgetUrl);
     return {
         hash: `${formatName(name as string)}-${object?.id}-${section?.id}-widget`,
         text: name as string,
+        value: text,
     };
 };
