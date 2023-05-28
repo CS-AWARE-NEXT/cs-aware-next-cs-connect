6a5e3e6d364552c0bf8469ca1ef7bef38537ed00
Tiziano <tizianocitro98@gmail.com>
2023-05-28 11:11:50 +0000

    Added graph to demo organization

--- cs-faker-data-provider/controller/graph.go
+++ cs-faker-data-provider/controller/graph.go
@@ -177,6 +177,72 @@ var graphMap = map[string]model.GraphData{
 		},
 		Description: graphDescription,
 	},
+	"4": {
+		Nodes: []model.GraphNode{
+			{
+				ID: "main-switch",
+				Position: model.GraphNodePosition{
+					X: 0,
+					Y: 0,
+				},
+				Data: model.GraphNodeData{
+					Kind:  model.Switch,
+					Label: "Main-Switch",
+				},
+			},
+			{
+				ID: "server-1",
+				Position: model.GraphNodePosition{
+					X: 200,
+					Y: -100,
+				},
+				Data: model.GraphNodeData{
+					Kind:  model.Server,
+					Label: "Server-1",
+				},
+			},
+			{
+				ID: "vpn-x",
+				Position: model.GraphNodePosition{
+					X: 200,
+					Y: 100,
+				},
+				Data: model.GraphNodeData{
+					Kind:  model.VpnServer,
+					Label: "VPN-X",
+				},
+			},
+			{
+				ID: "internet",
+				Position: model.GraphNodePosition{
+					X: 350,
+					Y: 100,
+				},
+				Data: model.GraphNodeData{
+					Kind:  model.Server,
+					Label: "Internet",
+				},
+			},
+		},
+		Edges: []model.GraphEdge{
+			{
+				ID:     "main-switch-server-1",
+				Source: "main-switch",
+				Target: "server-1",
+			},
+			{
+				ID:     "main-switch-vpn-x",
+				Source: "main-switch",
+				Target: "vpn-x",
+			},
+			{
+				ID:     "vpn-x-internet",
+				Source: "vpn-x",
+				Target: "internet",
+			},
+		},
+		Description: graphDescription,
+	},
 }
 
 var graphDescription = model.GraphDescription{
