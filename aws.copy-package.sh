fb025aa91cb858e8522bd04317a3d00975aa9668
Tiziano <tizianocitro98@gmail.com>
2023-11-23 10:13:59 +0000

    fix: minor hot update to copy script

--- aws.copy-package.sh
+++ aws.copy-package.sh
@@ -5,6 +5,6 @@ mkdir -p ~/cs-connect-packages
 docker cp cs-connect-base:/home/cs-aware-next-cs-connect/cs-connect/dist/cs-aware-connect-+.tar.gz \
     ~/cs-connect-packages/cs-aware-connect-+.tar.gz
 
-scp -i ~/cs-connect-demo.cs-aware.eu \
+scp -i ~/.ssh/isislab/cs-connect-demo.cs-aware.eu \
     ./cs-connect-packages/cs-aware-connect-+.tar.gz \
     ubuntu@cs-connect-demo.cs-aware.eu:/home/ubuntu/cs-aware-next-cs-connect/cs-connect/docker/package/cs-aware-connect-+.tar.gz
