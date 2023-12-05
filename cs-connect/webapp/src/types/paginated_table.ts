7dc78aeb2b1474910ddddb251a50c0a6cd513687
Tiziano <tizianocitro98@gmail.com>
2023-12-05 09:25:12 +0000

    feat: added default sorter for columns in paginated table widget

--- cs-connect/webapp/src/types/paginated_table.ts
+++ cs-connect/webapp/src/types/paginated_table.ts
@@ -8,7 +8,12 @@ export interface PaginatedTableColumn {
     key: string;
     title: string;
     width?: string;
+    sortable?: boolean;
     render?: (text: string, record: PaginatedTableRow) => JSX.Element;
+    sorter?: (a: PaginatedTableRow, b: PaginatedTableRow) => number;
 }
 
 export type PaginatedTableRow = any;
+
+export const NO_SORTER = false;
+export const WITH_SORTER = true;
