ab19b5e6254f7860ab4d58bb0fa745db7b5221a2
Tiziano <tizianocitro98@gmail.com>
2024-02-01 11:57:40 +0000

    feat: added issue flag to platform configuration

--- cs-connect/server/config/platform.go
+++ cs-connect/server/config/platform.go
@@ -37,6 +37,7 @@ type Organization struct {
 type Section struct {
 	ID       string    `json:"id" yaml:"id"`
 	Internal bool      `json:"internal" yaml:"internal"`
+	IsIssues bool      `json:"isIssues" yaml:"isIssues"`
 	Name     string    `json:"name" yaml:"name"`
 	URL      string    `json:"url" yaml:"url"`
 	Sections []Section `json:"sections" yaml:"sections"`
