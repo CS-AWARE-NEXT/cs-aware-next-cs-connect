9f851d2c0e4999a6c797bf2da89f0cb2d032d321
Tiziano <tizianocitro98@gmail.com>
2024-03-05 09:57:33 +0000

    feat: updated aws copy for package to cleaner version

--- aws.copy-package.sh
+++ aws.copy-package.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-mkdir -p ~/cs-connect-packages
+CONTAINER_NAME=cs-connect-base
+PACKAGE_NAME=cs-aware-connect-+.tar.gz
 
-docker cp cs-connect-base:/home/cs-aware-next-cs-connect/cs-connect/dist/cs-aware-connect-+.tar.gz \
-    ~/cs-connect-packages/cs-aware-connect-+.tar.gz
+HOST_TEMP_PACKAGE_DIR=./cs-connect-packages
+HOST_TEMP_PACKAGE=$HOST_TEMP_PACKAGE_DIR/$PACKAGE_NAME
 
+mkdir -p $HOST_TEMP_PACKAGE_DIR
+
+echo "Copying pluging from $CONTAINER_NAME to $HOST_TEMP_PACKAGE."
+docker cp $CONTAINER_NAME:/home/cs-aware-next-cs-connect/cs-connect/dist/$PACKAGE_NAME $HOST_TEMP_PACKAGE
+echo "Copy completed."
+
+echo "Remote copying pluging from $HOST_TEMP_PACKAGE to AWS."
 scp -i ~/.ssh/isislab/cs-connect-demo.cs-aware.eu \
-    ~/cs-connect-packages/cs-aware-connect-+.tar.gz \
-    ubuntu@cs-connect-demo.cs-aware.eu:/home/ubuntu/cs-aware-next-cs-connect/cs-connect/docker/package/cs-aware-connect-+.tar.gz
+    $HOST_TEMP_PACKAGE \
+    ubuntu@cs-connect-demo.cs-aware.eu:/home/ubuntu/cs-aware-next-cs-connect/cs-connect/docker/package/$PACKAGE_NAME
+echo "Remote copy completed."
