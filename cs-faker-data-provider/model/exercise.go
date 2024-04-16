7cbfb7c1cca8a28b72659c2b572d9bd388cf7c2a
Tiziano <tizianocitro98@gmail.com>
2024-04-16 08:24:28 +0000

    feat: added options for ocg courses

--- cs-faker-data-provider/model/exercise.go
+++ cs-faker-data-provider/model/exercise.go
@@ -10,11 +10,20 @@ type Assignment struct {
 	DescriptionName  string   `json:"descriptionName"`
 	DescriptionParts []string `json:"descriptionParts"`
 
+	InstructionName string   `json:"instructionName"`
+	Instructions    []string `json:"instructions"`
+
+	RegistrationAccessProcessName string   `json:"registrationAccessProcessName"`
+	RegistrationAccessProcess     []string `json:"registrationAccessProcess"`
+
 	AttackName  string   `json:"attackName"`
 	AttackParts []string `json:"attackParts"`
 
 	QuestionName string   `json:"questionName"`
 	Questions    []string `json:"questions"`
+
+	OpenQuestionName string   `json:"openQuestionName"`
+	OpenQuestions    []string `json:"openQuestions"`
 
 	EducationName     string   `json:"educationName"`
 	EducationMaterial []string `json:"educationMaterial"`
