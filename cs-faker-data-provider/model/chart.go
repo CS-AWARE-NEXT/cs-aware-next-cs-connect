dfdc6b90458b69b4fe52fba93e5fd0dec45d038f
Tiziano <tizianocitro98@gmail.com>
2024-05-13 14:38:33 +0000

    feat: supported other bar chart values

--- cs-faker-data-provider/model/chart.go
+++ cs-faker-data-provider/model/chart.go
@@ -27,10 +27,48 @@ type SimpleBarChartData struct {
 }
 
 type SimpleBarChartValue struct {
-	Label         string  `json:"label"`
-	NumberOfPosts float64 `json:"numberOfPosts"`
+	Label                     string  `json:"label"`
+	NumberOfPosts             float64 `json:"numberOfPosts"`
+	DureeMoyenneDeRechargeMin string  `json:"dureeMoyenneDeRechargeMin"`
+}
+
+type SimpleBarChart6Data struct {
+	BarData  []SimpleBarChart6Value `json:"barData"`
+	BarColor BarColor               `json:"barColor"`
+}
+
+type SimpleBarChart6Value struct {
+	Label                     string `json:"label"`
+	DureeMoyenneDeRechargeMin string `json:"dureeMoyenneDeRechargeMin"`
+}
+
+type SimpleBarChart3Data struct {
+	BarData  []SimpleBarChart3Value `json:"barData"`
+	BarColor BarColor               `json:"barColor"`
+}
+
+type SimpleBarChart3Value struct {
+	Label                       string `json:"label"`
+	HCPercentageConsummationkWH string `json:"hcPercentageConsummationkWH"`
+	HPPercentageConsummationkWH string `json:"hpPercentageConsummationkWH"`
+}
+
+type SimpleBarChart2Data struct {
+	BarData  []SimpleBarChart2Value `json:"barData"`
+	BarColor BarColor               `json:"barColor"`
+}
+
+type SimpleBarChart2Value struct {
+	Label             string `json:"label"`
+	HCConsummationkWH string `json:"hcConsummationkWH"`
+	HPConsummationkWH string `json:"hpConsummationkWH"`
 }
 
 type BarColor struct {
-	NumberOfPosts string `json:"numberOfPosts"`
+	NumberOfPosts               string `json:"numberOfPosts"`
+	DureeMoyenneDeRechargeMin   string `json:"dureeMoyenneDeRechargeMin"`
+	HCPercentageConsummationkWH string `json:"hcPercentageConsummationkWH"`
+	HPPercentageConsummationkWH string `json:"hpPercentageConsummationkWH"`
+	HCConsummationkWH           string `json:"hcConsummationkWH"`
+	HPConsummationkWH           string `json:"hpConsummationkWH"`
 }
