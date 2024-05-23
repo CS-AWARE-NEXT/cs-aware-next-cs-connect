3d3cd904ede496d9a7e9b830a1f6e8b26243522d
Tiziano <tizianocitro98@gmail.com>
2024-05-23 15:33:45 +0000

    feat: larissa fake incidents for scenario 1

--- cs-faker-data-provider/controller/incident_synthethic.go
+++ cs-faker-data-provider/controller/incident_synthethic.go
@@ -70,12 +70,21 @@ var incidentsSynthethicMap = map[string][]model.Incident{
 			Description: "The homepage of our WordPress website was hacked.",
 		},
 	},
+	"6": {
+		{
+			ID:          "64f3426b-d757-44fe-af73-ce8cef1be3af",
+			Name:        "Phishing Attack Stealing Credentials",
+			Description: "Employees were victims of a phshing attack stealing their credentials.",
+		},
+	},
 }
 
 var incidentsSynthethicTextBoxDataMap = map[string]string{
 	"03acf120-f08e-40c5-846e-b1c15d80e49e": `On May 23, 2024, the homepage of our WordPress website was hacked.
 	Unauthorized changes were detected, including the appearance of malicious content and redirects to an external site.
 	Multiple alerts were received from users indicating that the homepage was displaying unusual content and redirecting to a suspicious external website.`,
+	"64f3426b-d757-44fe-af73-ce8cef1be3af": `A phishing attack successfully tricked employees into revealing their login credentials.
+	Attackers gathered valuable information on interconnected organizations, setting the stage for further attacks.`,
 }
 
 var incidentsSynthethicTableData = model.TableData{
@@ -111,7 +120,7 @@ var incidentsSynthethicTableRowsMap = map[string][]model.TableRow{
 		},
 		{
 			ID:   "1435950a-af77-406d-9206-891ffacb1864",
-			Name: "ipv4-addr",
+			Name: "investigation",
 			Values: []model.TableValue{
 				{
 					Dim:   4,
@@ -148,6 +157,50 @@ var incidentsSynthethicTableRowsMap = map[string][]model.TableRow{
 				{
 					Dim:   8,
 					Value: `The site was restored from the most recent clean backup.`,
+				},
+			},
+		},
+	},
+	"64f3426b-d757-44fe-af73-ce8cef1be3af": {
+		{
+			ID:   "cc5fc224-8ce6-4ec4-a2c2-e356fabb4141",
+			Name: "detection",
+			Values: []model.TableValue{
+				{
+					Dim:   4,
+					Value: "detection",
+				},
+				{
+					Dim:   8,
+					Value: `Suspicious messages were received by employees under the name of their colleagues.`,
+				},
+			},
+		},
+		{
+			ID:   "919baa62-6c85-4fdf-9dfc-8a6d35f226ef",
+			Name: "investigation",
+			Values: []model.TableValue{
+				{
+					Dim:   4,
+					Value: "investigation",
+				},
+				{
+					Dim:   8,
+					Value: `Internal analysis revealed employees were victims of a phishing attack.`,
+				},
+			},
+		},
+		{
+			ID:   "271e5908-328b-45a7-a570-b06cee3168f1",
+			Name: "action",
+			Values: []model.TableValue{
+				{
+					Dim:   4,
+					Value: "action",
+				},
+				{
+					Dim:   8,
+					Value: `All compromised accounts were disabled in favor of the creation of new ones.`,
 				},
 			},
 		},
