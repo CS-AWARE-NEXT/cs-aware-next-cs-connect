6a10000639d33c554e3ea674a1886016153c61b8
Tiziano <tizianocitro98@gmail.com>
2024-05-24 08:21:25 +0000

    feat: 5thype and deyal fake incidents for scenario 1

--- cs-faker-data-provider/controller/incident_synthethic.go
+++ cs-faker-data-provider/controller/incident_synthethic.go
@@ -74,7 +74,21 @@ var incidentsSynthethicMap = map[string][]model.Incident{
 		{
 			ID:          "64f3426b-d757-44fe-af73-ce8cef1be3af",
 			Name:        "Phishing Attack Stealing Credentials",
-			Description: "Employees were victims of a phshing attack stealing their credentials.",
+			Description: "Employees were victims of a phishing attack stealing their credentials.",
+		},
+	},
+	"7": {
+		{
+			ID:          "a67a0081-b6cd-4044-b3e0-d22183cfde35",
+			Name:        "Disclosure of Sensitive Information",
+			Description: "Sensitive information was disclosed to unauthorized parties.",
+		},
+	},
+	"8": {
+		{
+			ID:          "1b105086-1d72-40ac-848a-3f43e6ab80b7",
+			Name:        "Leakage of Staff Information",
+			Description: "Employees were victims of a social engineering attack.",
 		},
 	},
 }
@@ -85,6 +99,10 @@ var incidentsSynthethicTextBoxDataMap = map[string]string{
 	Multiple alerts were received from users indicating that the homepage was displaying unusual content and redirecting to a suspicious external website.`,
 	"64f3426b-d757-44fe-af73-ce8cef1be3af": `A phishing attack successfully tricked employees into revealing their login credentials.
 	Attackers gathered valuable information on interconnected organizations, setting the stage for further attacks.`,
+	"1b105086-1d72-40ac-848a-3f43e6ab80b7": `Emails crafted to appear as urgent municipal updates lead to the leakage of senstive information about hospital staff.
+	Emails and messages were sent by impersonating people from the municipality.
+	The breach resulted in the exposure of sensitive data, creating potential risks of identity theft.`,
+	"a67a0081-b6cd-4044-b3e0-d22183cfde35": `Attackers impersonating colleagues from the municipality sent emails to employees, tricking them into revealing sensitive information.`,
 }
 
 var incidentsSynthethicTableData = model.TableData{
@@ -201,6 +219,66 @@ var incidentsSynthethicTableRowsMap = map[string][]model.TableRow{
 				{
 					Dim:   8,
 					Value: `All compromised accounts were disabled in favor of the creation of new ones.`,
+				},
+			},
+		},
+	},
+	"1b105086-1d72-40ac-848a-3f43e6ab80b7": {
+		{
+			ID:   "06bc7983-f586-4215-bf08-d44d156fc9e2",
+			Name: "investigation",
+			Values: []model.TableValue{
+				{
+					Dim:   4,
+					Value: "investigation",
+				},
+				{
+					Dim:   8,
+					Value: `Internal investigation revealed employees received messages impersonating people from the municipality.`,
+				},
+			},
+		},
+		{
+			ID:   "0bbb8377-5d50-4aa5-b837-11a780882699",
+			Name: "action",
+			Values: []model.TableValue{
+				{
+					Dim:   4,
+					Value: "action",
+				},
+				{
+					Dim:   8,
+					Value: `All passwords were updated following the leakage.`,
+				},
+			},
+		},
+	},
+	"a67a0081-b6cd-4044-b3e0-d22183cfde35": {
+		{
+			ID:   "7e360405-49f4-4d92-bdcf-078320264290",
+			Name: "detection",
+			Values: []model.TableValue{
+				{
+					Dim:   4,
+					Value: "detection",
+				},
+				{
+					Dim:   8,
+					Value: `Employees received emails impersonating people from the municipality.`,
+				},
+			},
+		},
+		{
+			ID:   "e5bf09af-c042-478a-9973-8dff95c52a58",
+			Name: "investigation",
+			Values: []model.TableValue{
+				{
+					Dim:   4,
+					Value: "investigation",
+				},
+				{
+					Dim:   8,
+					Value: `Employees were tricked into revealing sensitive information.`,
 				},
 			},
 		},
