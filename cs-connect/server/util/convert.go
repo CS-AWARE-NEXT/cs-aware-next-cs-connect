aa49ef10c0f3fc82eef94e901dca3fee3d1604dc
Tiziano <tizianocitro98@gmail.com>
2024-11-07 09:12:35 +0000

    feat: introduced convertion unix milli to utc for stix compliance

--- cs-connect/server/util/convert.go
+++ cs-connect/server/util/convert.go
@@ -1,6 +1,9 @@
 package util
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"time"
+)
 
 func Convert(a, b interface{}) error {
 	js, err := json.Marshal(a)
@@ -8,4 +11,10 @@ func Convert(a, b interface{}) error {
 		return err
 	}
 	return json.Unmarshal(js, b)
+}
+
+func ConvertUnixMilliToUTC(timestamp int64) string {
+	// format := time.RFC3339
+	format := "2006-01-02T15:04:05.000000Z"
+	return time.UnixMilli(timestamp).UTC().Format(format)
 }
