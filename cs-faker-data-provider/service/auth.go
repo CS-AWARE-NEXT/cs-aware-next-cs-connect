e79afbbbb5737e02c6329e591a7bd178c149de20
Tiziano <tizianocitro98@gmail.com>
2024-11-18 13:06:37 +0000

    feat: added auth service to get api token

---
+++ cs-faker-data-provider/service/auth.go
@@ -0,0 +1,75 @@
+package service
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gofiber/fiber/v2/log"
+)
+
+type AuthResponse struct {
+	AccessToken string `json:"access_token"`
+	IdToken     string `json:"id_token"`
+}
+
+func (ar AuthResponse) String() string {
+	return fmt.Sprintf("AccessToken: %s, IdToken: %s", ar.AccessToken, ar.IdToken)
+}
+
+type AuthService struct {
+	endpoint string
+}
+
+func NewAuthService(endpoint string) *AuthService {
+	return &AuthService{
+		endpoint: endpoint,
+	}
+}
+
+func (ac *AuthService) Auth(username, password string) (AuthResponse, error) {
+	log.Infof("Authenticating user [%s] at %s", username, ac.endpoint)
+	queryString := fmt.Sprintf("email=%s&password=%s", username, password)
+	req, err := http.NewRequest(
+		"POST",
+		ac.endpoint+"?"+queryString,
+		nil,
+	)
+	if err != nil {
+		log.Error("error creating request ", err.Error())
+		return AuthResponse{}, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	transport := &http.Transport{}
+	client := &http.Client{Transport: transport}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Error("error requesting news posts ", err.Error())
+		return AuthResponse{}, err
+	}
+	defer resp.Body.Close()
+	defer transport.CloseIdleConnections()
+
+	log.Info("Response Status: ", resp.Status)
+	log.Info("Response Headers: ", resp.Header)
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Error("error reading response body ", string(respBody), err.Error())
+		return AuthResponse{}, err
+	}
+	log.Info("Auth response Body: ", string(respBody))
+
+	log.Info("unmarshaling auth response")
+	var response AuthResponse
+	err = json.NewDecoder(bytes.NewReader(respBody)).Decode(&response)
+	if err != nil {
+		log.Error("error unmarshaling news posts ", err.Error())
+		return AuthResponse{}, err
+	}
+
+	return response, nil
+}
