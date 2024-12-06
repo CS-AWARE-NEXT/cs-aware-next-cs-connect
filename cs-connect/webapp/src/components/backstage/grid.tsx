9084327184462e0387c6f330d8e68d405bf2e774
Tiziano <tizianocitro98@gmail.com>
2024-12-06 11:05:40 +0000

    feat: added border box to grid

--- cs-connect/webapp/src/components/backstage/grid.tsx
+++ cs-connect/webapp/src/components/backstage/grid.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 
 export const HorizontalSplit = styled.div`
     display: block;
@@ -26,4 +26,13 @@ export const HorizontalSeparator = styled.div`
     border-top: 1px solid rgba(var(--center-channel-color-rgb), 0.16);
     margin: 16px 0;
     width: 100%;
+`;
+
+export const BorderBox = styled.div<{border?: boolean}>`
+    padding: 8px;
+    border-radius: 8px;
+
+    ${({border}) => border && css`
+        border: 1px solid rgba(var(--center-channel-color-rgb), 0.16);
+    `}
 `;
