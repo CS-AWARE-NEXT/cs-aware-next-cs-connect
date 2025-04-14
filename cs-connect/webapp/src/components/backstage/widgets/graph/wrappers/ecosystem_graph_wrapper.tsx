86e9c4b0f23fc3be28667c6671600f7335661e6f
Tiziano <tizianocitro98@gmail.com>
2025-04-14 13:06:12 +0000

    feat: export url is now passed to the editable graph view

--- cs-connect/webapp/src/components/backstage/widgets/graph/wrappers/ecosystem_graph_wrapper.tsx
+++ cs-connect/webapp/src/components/backstage/widgets/graph/wrappers/ecosystem_graph_wrapper.tsx
@@ -298,6 +298,7 @@ const EcosystemGraphWrapper = ({
                         }}
                         lockStatus={lockStatus}
                         refreshNodeInternals={refreshNodeInternals}
+                        exportUrl={`${url}/export`}
                     />
                 ) : (
                     <Graph
